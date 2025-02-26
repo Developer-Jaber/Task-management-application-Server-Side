059c55d1094dd726c0153e1f56f3e04f04a1b3f1
Developer-Jaber <jaberhossain.web.developer@gmail.com>
2025-02-26 10:53:31 +0400

    Add API enpoint for post and grt user data.

--- index.js
+++ index.js
@@ -16,27 +16,42 @@ const uri = `mongodb+srv://${process.env.USERDB}:${process.env.USERPASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // all colection
+        const userCollection = client.db('TaskManagementProject').collection('user')
+
+        // create api for posting user data
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+        // create api for getting all user data
+        app.get('/user', async (req, res) => {
+            const participet = req.body;
+            const result = await userCollection.find(participet).toArray();
+            res.send(result);
+        })
 
 
-    
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
